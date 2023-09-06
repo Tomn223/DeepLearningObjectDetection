860d2ae6ff4b3b325f49c915ebf86c2820d73219
Tomn223 <nemcek.8@osu.edu>
2023-09-05 22:26:30 -0400

    Confidence tests, cleanup

--- detect.py
+++ detect.py
@@ -1,15 +1,13 @@
 import cv2
 import torch
-import pandas
 import os
 
-# vehicles_list = ["car", "truck", "bus", "motorcycle", "bicycle"]
+# YOLOv5 class/category number ranges by supercategory
+VEHICLES_RANGE = [1,8]
+PERSON_NUM = 0
+ANIMALS_RANGE = [14, 23]
 
-# YOLO v5 class/category number ranges by supercategory
-vehicles_range = [1,8]
-person_num = 0
-animals_range = [14, 23]
-
+# how confident model must be to display object bounding box
 CONFIDENCE_THRESHOLD = 0.5
 
 class Detector():
@@ -24,16 +22,16 @@ class Detector():
     def predict(self, img):
         return self.model(img)
     
-    def plot_boxes(self, results, img):
+    def plot_boxes(self, results, img, confidence_ovr=None):
         for box in results.xyxy[0]:
             x1, y1, x2, y2, confidence, class_num = box
             x1, y1, x2, y2, class_num = int(x1), int(y1), int(x2), int(y2), int(class_num)
 
-            if class_num == person_num:
+            if class_num == PERSON_NUM:
                 color = (0,255,0)
-            elif class_num >= vehicles_range[0] and class_num <= vehicles_range[1]:
+            elif class_num >= VEHICLES_RANGE[0] and class_num <= VEHICLES_RANGE[1]:
                 color = (0,0,255)
-            elif class_num >= animals_range[0] and class_num <= animals_range[1]:
+            elif class_num >= ANIMALS_RANGE[0] and class_num <= ANIMALS_RANGE[1]:
                 color = (255, 0, 0)
             else:
                 # only detecting vehicles, pedestrians & animals
@@ -41,7 +39,10 @@ class Detector():
 
             class_name = self.CLASS_NAMES_DICT[class_num]
 
-            if confidence > CONFIDENCE_THRESHOLD:
+            # needed for confidence tests
+            conf_threshold = confidence_ovr if confidence_ovr is not None else CONFIDENCE_THRESHOLD
+
+            if confidence > conf_threshold:
                 cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)  # bounding box
                 size_text, _ = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_PLAIN, 1, 2)
                 w_text, h_text = size_text
@@ -49,16 +50,36 @@ class Detector():
                 cv2.putText(img, class_name, (x1, y1-5+h_text), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 2)  # label text
 
         return img
-    
+
+def all_image_object_detection(object_detector, image_dir, save_dir):
+
+    for image_file in os.listdir(image_dir):
+        img = cv2.imread(image_dir + image_file)
+        results = object_detector.predict(img)
+        img = object_detector.plot_boxes(results, img)
+        cv2.imwrite(save_dir + image_file, img)
+        cv2.imshow("img", img)
+        cv2.waitKey(0)
+
+def confidence_tests(object_detector, image_file, save_dir, conf_test_list):
+
+    for conf_threshold in conf_test_list:
+        img = cv2.imread(image_dir + image_file) # to prevent overwriting same image
+        results = object_detector.predict(img)
+        img = object_detector.plot_boxes(results, img, confidence_ovr=conf_threshold)
+        cv2.imwrite(save_dir + str(conf_threshold) + "-conf" + image_file, img)
+        cv2.imshow("img", img)
+        cv2.waitKey(0)
 
 object_detector = Detector()
 
 image_dir = os.path.join(os.getcwd(), "images") + "\\"
 results_dir = os.path.join(os.getcwd(), "results") + "\\"
-for image_file in os.listdir(image_dir):
-    img = cv2.imread(image_dir + image_file)
-    results = object_detector.predict(img)
-    img = object_detector.plot_boxes(results, img)
-    cv2.imwrite(results_dir + image_file, img)
-    cv2.imshow("img", img)
-    cv2.waitKey(0)
+confidence_test_dir = os.path.join(os.getcwd(), "conf_test_results") + "\\"
+
+all_image_object_detection(object_detector, image_dir, results_dir) 
+
+confidence_test_list = [0.1, 0.25, 0.5, 0.75, 0.9]
+confidence_tests(object_detector, "1.jpg", confidence_test_dir, confidence_test_list)
+
+
